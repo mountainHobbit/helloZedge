bd7f5f176d90533952845fe1cf135e8fdbe33b9d
Ben Schreiber <ben.schreiber.no@gmail.com>
2020-10-10 10:35:53 +0200

    change to v2

--- hello-app/main.go
+++ hello-app/main.go
@@ -45,9 +45,9 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.7\n")
+	fmt.Fprintf(w, "Version: 2.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
-	fmt.Fprintf(w, "This is an even better test.\n")
+	fmt.Fprintf(w, "This is the best test!.\n")
 }
 
 // [END container_hello_app]
